4c886263151dc5513fd1b1492318e85b001f4192
haotian <wong.haotian@foxmail.com>
2021-09-13 11:44:59 +0800

    update

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -9,12 +9,7 @@ VERSION = $(cat /etc/issue |sed -n "1,1p"| awk '{print $2}'|cut -d '.' -f 1,2)
 if [ "${CH_SRC}" == "1" ]; then
     if["${VERSION}" == "20.04"]; then
         echo
-
-    # sudo apt-get autoclean
-    # sudo apt-get clean
-    # sudo apt-get update
-    # sudo apt-get upgrade
-    # sudo apt-get autoremove
+    
     sudo apt autoclean
     sudo apt clean
     sudo apt update
@@ -22,24 +17,38 @@ if [ "${CH_SRC}" == "1" ]; then
     sudo apt autoremove -y
 fi
 
+# Install Git
+if [ "${GIT}" == "1" ]; then
+    sudo apt install -y git
+    git --version
+    git config --global user.name "${GIT_USER}"
+    git config --global user.email "${GIT_EMAIL}"
+    git config --list
+fi
+
+# Install SSH
+if [ "${SSH }" == "1" ]; then
+    sudo apt install -y ssh
+fi
+
 # Install Pip
 if [ "${PIP}" == "1" ]; then
-    sudo apt-get install -y python3-pip
+    sudo apt install -y python3-pip
 fi
 
 # Install Visual Studio Code
 if [ "${VSCODE}" == "1" ]; then
-    sudo apt install software-properties-common apt-transport-https wget
+    sudo apt install -y software-properties-common apt-transport-https wget
     wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-    sudo apt-get insatll -y code
+    sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt install -y code
 fi
 
 # Install Typora
 if [ "${TYPORA}" == "1" ]; then
     wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-    sudo add-apt-repository 'deb https://typora.io/linux ./'
-    sudo apt-get install -y typora
+    sudo add-apt-repository -y 'deb https://typora.io/linux ./'
+    sudo apt install -y typora
 fi
 
 # Install OpenJDK
@@ -54,7 +63,7 @@ if [ "${IDEA}" == "1" ]; then
 if [ "${CLASH}" == "1" ]; then
     mkdir ~/.config/clash
     wget https://github.com/Dreamacro/clash/releases/download/v1.7.0/clash-linux-amd64-v1.7.0.gz
-    sudo apt-get install -y gzip
+    sudo apt install -y gzip
     gzip -d clash-linux-amd64-v1.7.0.gz
     mv clash-linux-amd64-v1.7.0.gz ~/.config/clash/clash
     cd ~/.config/clash
@@ -76,5 +85,5 @@ fi
 
 # Install Python Models
 if [ "${PY_MODEL}" == "1" ]; then
-
+    pip install -r -y requirements.txt
 fi
